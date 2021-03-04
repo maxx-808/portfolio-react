00e002de727ba7727b70ed0fedf809a91f533221
Maxx <maxhiga.hawaii@gmail.com>
2021-03-03 23:04:58 -1000

    updating code to have a skeleton of the app added navbar elements

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
+import "./App.css";
 import { HashRouter, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <HashRouter>
+    <HashRouter basename="/">
+      <div className="App">
+        <Header />
+
         <Route exact path="/" component={Home} />
-      </HashRouter>
-      <Footer />
-    </div>
+
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,44 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header>
-      <h1>hello from Header</h1>
-    </header>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Employee Directory
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <p id="sort" className="nav-link">
+              Sort By:
+            </p>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/g">
+              Gender
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/l">
+              Last Name
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
