3b38ff53aa738c11cbd46a89bc4ab82696b0a8eb
Maxx <maxhiga.hawaii@gmail.com>
2021-03-04 14:45:02 -1000

    starting project component

---
+++ src/pages/ProjectPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import projects from "../assets/projects.json";
+import Project from "../components/Project";
+
+const ProjectPage = () => {
+  return <div></div>;
+};
+
+export default ProjectPage;
