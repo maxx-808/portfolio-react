fde770841d65cfaec68b3bf36c29037ff2faa4c7
Maxx <maxhiga.hawaii@gmail.com>
2021-04-02 16:03:55 -1000

    changed about me page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,23 +3,44 @@ const Home = () => {
   return (
     <div className="container about-container">
       <h1>About Me</h1>
+      <br />
+      <div className="card">
+        <p>Hi, my name is Max Higa, born and raised on Oahu, Hawaii.</p>
+        <p>
+          For the past 5 or so years I have been fascinated by computers and the
+          internet, wondering how people can actually make electronics and
+          technology and how the wireless world works. I got my first gaming
+          computer in 2017 from saving up money at the job I was working at and
+          my love for technology never stopped. Due to covid, it felt like I had
+          a chance to pause life for a bit and think of what I want to do. Being
+          introduced to a class that is solely for the subject you want to learn
+          and being able to show your worth through your own work appealed to me
+          and made me realize that I could possibly work in creating things of
+          the internet which always interested me. Starting to code has opened a
+          whole new world for me.
+        </p>
+      </div>
+
+      <h4>Work Experience</h4>
       <div className="card">
         <p>
-          Currently enrolled in a full stack coding bootcamp from UC Berkeley
-          Extension. This class is set to end on March 27, 2021 and I started
-          during the ending of September 2020. Through doing this class I have
-          learned a lot about web development and coding as a whole. My goal
-          with this class is to learn about the coding world and start my
-          journey through a career with computers.
+          I have worked at Consolidated Theatres Mililani for just under 5
+          years. My work there includes everything from cleaning and customer
+          service, to leading the kitchen, managing stock and orders, as well as
+          leading the team throughout the shift. I have left this job to focus
+          fully on my coding class that I just finished through UC Berkeley
+          Extension for full stack web development.
         </p>
         <p>
           So far, I have learned a myriad of skills for web development based on
           JavaScript. Mostly sticking to HTML, CSS, and JavaScript; we have
-          expanded into their libraries and attached programs and styles.
+          expanded into their libraries and attached programs and styles. We
+          have mainly focused on Reactjs, express, Node.js, mongoDB and MySQL to
+          create full stack applications.
         </p>
       </div>
       <br />
-      <h4>Some things I have learned so far:</h4>
+      <h4>What I have learned:</h4>
       <div className="card">
         <ul>
           <li>Basics of HTML, CSS, JavaScript</li>
@@ -38,14 +59,22 @@ const Home = () => {
         </ul>
         <br />
         <p>
-          Before this class, I had no experience in coding. For this class I had
-          some prework to do and i got a free online intro course into coding.
-          Other than that, I was working at a movie theater since the middle of
-          highschool. I went up from doing cashier and cleaning work to becoming
-          an assistant manager doing tasks like inventory, logging info in excel
-          sheets, and much more. I've had quite the diverse work history working
-          there and it has helped me to have a good work ethic during the class
-          I am currently in.
+          Before this class, I was working at a movie theater since the middle
+          of highschool. I went up from doing cashier and cleaning work to
+          becoming an assistant manager doing tasks like inventory, logging info
+          in excel sheets, and much more. I've had quite the diverse work
+          history working there and it has helped me to have a good work ethic
+          during the class I am currently in.
+        </p>
+      </div>
+      <br />
+      <h4>What am I doing now: </h4>
+      <div className="card">
+        <p>
+          Now that I have completed this course, I am actively looking for a job
+          in the industry. I am also constantly studying and learning more about
+          web development and software engineering as well as working on
+          personal projects.
         </p>
       </div>
     </div>
